7be18af56bf99f13de5a3d08be410a272b7999f5
Little Lucas" Rodrigues Oronha <contatolucasruki@gmail.com>
2026-07-12 18:01:38 -0300

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://portfolio-design-2026.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
